db454fcce2dad49b991c17140e3f0907a5af62cd
Anastasija Semen <anastasija.semen@gmail.com>
2018-09-12 10:56:12 +0300

    add carousel

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
+
+let slideIndex = 0;
+showCarousel();
+
+function showCarousel() {
+    let i;
+    let slides = document.getElementsByClassName("slidesmain_fade");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showCarousel, 3000); 
+}
+
 function initMap() {
 
     var centerLatLng = new google.maps.LatLng(56.2928515, 43.7866641);
@@ -9,3 +25,4 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
 google.maps.event.addDomListener(window, "load", initMap);
+
