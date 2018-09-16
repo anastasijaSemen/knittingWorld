1731d48aa8eb87fca9d91055108941de09a0af52
Anastasija Semen <anastasija.semen@gmail.com>
2018-09-16 14:37:56 +0300

    change name of json

--- js/index.js
+++ js/index.js
@@ -6,33 +6,33 @@ function showCarousel() {
     let i;
     let slides = document.getElementsByClassName("slidesmain_fade");
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
+        slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showCarousel, 3000); 
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showCarousel, 3000);
 }
 
 
 
 function initMap() {
 
-    var centerLatLng = new google.maps.LatLng(56.2928515, 43.7866641);
+    let centerLatLng = new google.maps.LatLng(56.2928515, 43.7866641);
 
-    var mapOptions = {
+    let mapOptions = {
         center: centerLatLng,
         zoom: 8
     };
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    let map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
 google.maps.event.addDomListener(window, "load", initMap);
 
 
 
 // smooth scroll
-$(function() {
-    $('.smooth').on('click', function(event) {
+$(function () {
+    $('.smooth').on('click', function (event) {
         var target = $(this.getAttribute('href'));
         if (target.length) {
             event.preventDefault();
@@ -44,38 +44,20 @@ $(function() {
 });
 
 // imitation of backend. Data from JSON to section our projects
-document.querySelector(".woman_sweater").addEventListener("click", f1)
-document.querySelector(".green_dress").addEventListener("click", f2)
-document.querySelector(".broun_scarf").addEventListener("click", f3)
-document.querySelector(".green_heat").addEventListener("click", f4)
-document.querySelector(".knit_for_kids").addEventListener("click", f5)
+document.querySelector(".woman_sweater").addEventListener("click", () => fetchProjectJson(woman_sweater))
+document.querySelector(".green_dress").addEventListener("click", () => fetchProjectJson(green_dress))
+document.querySelector(".broun_scarf").addEventListener("click", () => fetchProjectJson(broun_scarf))
+document.querySelector(".green_heat").addEventListener("click", () => fetchProjectJson(green_heat))
+document.querySelector(".knit_for_kids").addEventListener("click", () => fetchProjectJson(knit_for_kids))
 
-function f1(){
-    fetchProjectJson(1)
-}
-
-function f2(){
-    fetchProjectJson(2)
-}
-function f3(){
-    fetchProjectJson(3)
-}
-function f4(){
-    fetchProjectJson(4)
-}
-function f5(){
-    fetchProjectJson(5)
-}
-
-
-function fetchProjectJson(i){
-    fetch('json/'+i+'.json')
-    .then(data => data.json())
-    .then(json => {
-        const projectsContainer = document.querySelector(".projects_container");
-        projectsContainer.innerHTML = 
-        "<div>"+json.title+"<img src=\""+json.bigPictureAddress+"\"/></div>"
-    });
+function fetchProjectJson(i) {
+    fetch('json/' + i + '.json')
+        .then(data => data.json())
+        .then(json => {
+            const projectsContainer = document.querySelector(".projects_container");
+            projectsContainer.innerHTML =
+                `<div class="json">${json.title}<img src="${json.bigPictureAddress}"/></div>`
+        });
 }
 
 
